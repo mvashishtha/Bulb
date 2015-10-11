b537c83941bb248e4c3bbaa86b19cc1b28791028
unknown <Joey>
2015-10-11 00:17:18 -0700

    css and html for landing page

--- app.js
+++ app.js
@@ -100,6 +100,8 @@ app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }))
  * Primary app routes.
  */
 app.get('/', homeController.index);
+// app.get('/twenty', homeController.twenty);
+
 app.get('/login', userController.getLogin);
 app.post('/login', userController.postLogin);
 app.get('/logout', userController.logout);
